a7b04c5e155f3219fde30fdaaac7a60015f56702
gerardb83 <gbreitenbeck@gmail.com>
2020-03-02 22:20:09 -0500

    added andExpect view name to first test

--- src/test/java/co/grandcircus/HelpMeTest/HelpMeTestApplicationTests.java
+++ src/test/java/co/grandcircus/HelpMeTest/HelpMeTestApplicationTests.java
@@ -1,32 +1,69 @@
 package co.grandcircus.HelpMeTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class HelpMeTestApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
 	@Autowired
 	private HelpMeTestController controller;
 	
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 
+//	@MockBean
+//	private RegisterUseCase registerUseCase;
+	
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/")).andExpect(status().isOk());
+	public void statusOkay() throws Exception {
+		this.mockMvc.perform(get("/"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("index"));
 	}
 	
 	@Test
 	public void contexLoads() throws Exception {
 		assertNotNull(controller);
 	}
+
+
+//	  @Test
+//	  void whenValidInput_thenReturns200() throws Exception {
+//	    mockMvc.perform(...);
+//	  }
+	
+//	@Test
+//	public void greetingShouldReturnDefaultMessage() throws Exception {
+//		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+//		MVC.class)).getClass("Hello, World");
+//		}
 }
